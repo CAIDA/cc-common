aab737ad9128f9974db06e62423a852cb0ecbc8e
Alistair King <alistair@caida.org>
2016-04-20 09:15:53 -0700

    Update parse_cmd to support nested quoted strings

--- parse_cmd.c
+++ parse_cmd.c
@@ -20,10 +20,23 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * 2016-04-20:
+ * Alistair updated to support nested quoted strings. The implementation is not
+ * perhaps the most efficient, but this function is hardly
+ * performance-critical. Basically it works by only considering a quoted word
+ * complete when it finds a quote that is **not** preceded by a backslash. It
+ * then deletes one backslash that precedes each quotation.
+ * I also "fixed" the formatting of the code.
+ */
+
+#include <string.h>
+
 static void skip_white(char **buf)
 {
-	while(**buf==' ')
-		(*buf)++;
+  while (**buf==' ') {
+    (*buf)++;
+  }
 }
 
 /* Get the next word in a line
@@ -38,63 +51,65 @@ static void skip_white(char **buf)
  *
  * ' foo bar baz' => 'foo' 'bar baz'
  * ' "foo bar" baz' => 'foo bar' ' baz'
+ * ' foo "bar \"baz baz\""' => 'foo' 'bar "baz baz"'
  */
 static char * split_cmd(char **buf)
 {
-	char *ret = 0;
+  char *ret = 0;
 
-	skip_white(buf);
+  skip_white(buf);
 
-	if (**buf=='"') /* Quoted */
-	{
-		(*buf)++;
-		ret=*buf;
+  if (**buf=='"') { /* Quoted */
+    (*buf)++;
+    ret=*buf;
 
-		while(**buf && **buf!='"')
-			(*buf)++;
+    while (**buf && (**buf!='"' || *((*buf)-1)=='\\')) {
+      if (**buf=='"' && *((*buf)-1)=='\\') { /* Nested quote */
+        memmove((*buf)-1, *buf, strlen(*buf));
+      } else {
+        (*buf)++;
+      }
+    }
 
-		if (**buf)
-		{
-			**buf='\0';
-			(*buf)++;
-		}
-	} else
-	{
-		ret=*buf;
+    if (**buf) {
+      **buf='\0';
+      (*buf)++;
+    }
+  } else {
+    ret=*buf;
 
-		while(**buf && **buf!=' ')
-			(*buf)++;
+    while (**buf && **buf!=' ') {
+      (*buf)++;
+    }
 
-		if (**buf)
-		{
-			**buf='\0';
-			(*buf)++;
-		}
-	}
-	return ret;
+    if (**buf) {
+      **buf='\0';
+      (*buf)++;
+    }
+  }
+  return ret;
 }
 
 /* Split a command line up into parc,parv
  * using command line rules
  */
-void parse_cmd(char *buf,int *parc, char *parv[], int MAXTOKENS,
+void parse_cmd(char *buf, int *parc, char *parv[], int MAXTOKENS,
 	       const char *command_name)
 {
-	int i=0;
-	parv[0] = (char*) command_name;
-	*parc=1;
+  int i=0;
+  parv[0] = (char*) command_name;
+  *parc=1;
 
-	while(*buf)
-	{
-		parv[(*parc)++]=split_cmd(&buf);
-		if (*parc>(MAXTOKENS-1))
-		{
-			parv[*parc]=buf;
-			break;
-		}
-	}
-	for(i=*parc;i<MAXTOKENS;i++)
-		parv[i]="";
+  while(*buf) {
+    parv[(*parc)++] = split_cmd(&buf);
+    if (*parc>(MAXTOKENS-1)) {
+      parv[*parc]=buf;
+      break;
+    }
+  }
+  for (i = *parc; i<MAXTOKENS; i++) {
+    parv[i]="";
+  }
 }
 
 
